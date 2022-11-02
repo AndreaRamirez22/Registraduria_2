fbf27017d1933d72974806f00dfec5d7fd1e5095
AndreaRamirez22 <117320657+AndreaRamirez22@users.noreply.github.com>
2022-11-02 17:56:14 -0500

    Add files via upload

---
+++ ControladorResultado.py
@@ -0,0 +1,70 @@
+from Modelos.Resultado import Resultado
+from Modelos.Mesa import Mesa
+from Modelos.Candidato import Candidato
+from Repositorios.RepositorioResultado import RepositorioResultado
+from Repositorios.RepositorioMesa import RepositorioMesa
+from Repositorios.RepositorioCandidato import RepositorioCandidato
+
+
+class ControladorResultado():
+    def __init__(self):
+        self.repositorioResultado = RepositorioResultado()
+        self.repositorioMesas = RepositorioMesa()
+        self.repositorioCandidatos = RepositorioCandidato()
+    def index(self):
+        return self.repositorioResultado.findAll()
+    """
+    Asignacion Mesa y Candidato a resultado
+    """
+    def create(self,infoResultado,id_Mesa,id_Candidato):
+        nuevaResultado=Resultado(infoResultado)
+        laMesa=Mesa(self.repositorioMesas.findById(id_Mesa))
+        elcandidato=Candidato(self.repositorioCandidatos.findById(id_Candidato))
+        nuevaResultado.Mesa=laMesa
+        nuevaResultado.Candidato=elcandidato
+        return self.repositorioResultado.save(nuevaResultado)
+    def show(self,id):
+        elResultado=Resultado(self.repositorioResultado.findById(id))
+        return elResultado.__dict__
+    """
+    Modificación de Resultado (Mesa y Candidato)
+    """
+    def update(self,id,infoResultado,id_Mesa,id_Candidato):
+        elResultado=Resultado(self.repositorioResultado.findById(id))
+        elResultado.numero_mesa=infoResultado["numero_mesa"]
+        elResultado.cedula_candidato = infoResultado["cedula_candidato"]
+        elResultado.notaFinal=infoResultado["numero_votos"]
+        laMesa = Mesa(self.repositorioMesas.findById(id_Mesa))
+        elcandidato = Candidato(self.repositorioCandidatos.findById(id_Candidato))
+        elResultado.Mesa = laMesa
+        elResultado.Candidato = elcandidato
+        return self.repositorioResultado.save(elResultado)
+    def delete(self, id):
+        return self.repositorioResultado.delete(id)
+
+    "Obtener todos los Resultados de un Candidato"
+    def listarResultadosEnCandidato(self,id_Candidato):
+        return self.repositorioResultado.getListadoResultadosEnCandidato(id_Candidato)
+
+    "Obtener todos los Resultados por Mesa"
+    def listarResultadosporMesa(self,id_Mesa):
+        return self.repositorioResultado.getListadoResultadosporMesa(id_Mesa)
+
+    "Obtener votos mas altos por candidato"
+    def votosmasAltosPorCandidato(self):
+            return self.repositorioResultado.getMayorvotoCandidato()
+
+    "Obtener Total por Candidato"
+
+    def sumaMesasxCandidato(self):
+        return self.repositorioResultado.getTotalVotosxCandidato()
+
+
+    "Obtener votos mas altos por candidato"
+    def votosmasAltosPorCandidato(self):
+            return self.repositorioResultado.getMesastotalxCandidatos()
+
+
+    "Obtener promedio de notas en Candidato"
+    def promedioNotasEnCandidato(self,id_Candidato):
+        return self.repositorioResultado.promedioNotasEnCandidato(id_Candidato)
